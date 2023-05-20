10c8a3e03d3e5757bae954bcfb81697cbcab9bec
irfan <23pointalpha@gmail.com>
2023-05-20 17:18:44 +0300

    Spring Core Spring Container Bean annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -0,0 +1,20 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
+
+        FullTimeMentor ft = container.getBean(FullTimeMentor.class);
+
+        ft.createAccount();
+
+        String str = container.getBean(String.class);
+        System.out.println(str);
+
+    }
+}
