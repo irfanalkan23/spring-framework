2e44c50ee85edcbaf8816b3ff44e4d0fc93844b7
irfan <23pointalpha@gmail.com>
2023-05-21 23:20:27 +0300

    Spring Core Spring Container Wiring; direct wiring, autowiring

---
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/ConfigCar.java
@@ -0,0 +1,34 @@
+package com.cydeo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigCar {
+
+    @Bean
+    Car car(){
+        Car c = new Car();
+        c.setMake("Honda");
+        return c;
+    }
+
+    //Direct wiring
+//    @Bean
+//    Person person(){
+//        Person p = new Person();
+//        p.setName("Mike");
+//        p.setCar(car());
+//        return p;
+//    }
+
+    //Auto wiring
+    @Bean
+    Person person(Car car){
+        Person p = new Person();
+        p.setName("Mike");
+        p.setCar(car);
+        return p;
+    }
+
+}
