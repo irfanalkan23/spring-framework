212c934ad697ebe5e1ae5c165b0ae3128de551a8
irfan <23pointalpha@gmail.com>
2023-05-24 22:53:33 +0300

    Spring Core Abstraction Comment feature scenario

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
@@ -0,0 +1,10 @@
+package com.cydeo.proxy;
+
+import com.cydeo.model.Comment;
+
+public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending notification for comment : " + comment.getText());
+    }
+}
